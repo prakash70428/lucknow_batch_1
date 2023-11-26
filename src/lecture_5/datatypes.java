20d2195fdf070f0d36943520ae35113cfd06df7d
prakash <prakashmendish@gmail.com>
2023-11-26 23:38:30 +0530

    first commit

---
+++ src/lecture_5/datatypes.java
@@ -0,0 +1,11 @@
+package lecture_5;
+import java.util.*;
+public class datatypes {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        
+        
+	}
+}
